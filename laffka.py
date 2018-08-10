5024fb932f12891376a9f326805acb662cc04576
sginne <sigin@norsula.com>
2018-08-10 22:54:51 +0300

    Logic of ports corrected.

--- laffka.py
+++ laffka.py
@@ -7,6 +7,6 @@ try:
     if Configuration.debugging==True:
         app.run(host='127.0.0.1',port=Configuration.port)
     else:
-        serve(app,host='127.0.0.1', port=5000)
+        serve(app,host='127.0.0.1', port=Configuration.port)
 except AttributeError:
     serve(app, host='127.0.0.1', port=5000)
