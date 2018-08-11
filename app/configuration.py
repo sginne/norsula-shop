c389ec6af5db2dabddd4e92796939a8dadd08574
sginne <sigin@norsula.com>
2018-08-11 18:48:06 +0300

    Update configuration.py
    for release

--- app/configuration.py
+++ app/configuration.py
@@ -1,17 +1,17 @@
 class Configuration():
-    user='123'
-    password='234'
+    user=''
+    password=''
+    btc_master_key = ''
 
     database_url = './db/main'
     wtf_csrf = True
     salt = 'salt'
     btc_rate_url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR'
-    btc_master_key = 'belt arrange uphold soul dinosaur meat pave version heavy used spatial skin'
     shop_name="Laffkashop"
     shop_url="laffka6wwduoexvb.onion"
     btc_net = "Main"  # Test for testnet, otherwise MainNet
     debugging = True
-    port=5000 #Port to serve for debugging or no. if not set, 5000 applied
+    port=5678 #Port to serve for debugging or no. if not set, 5000 applied
     update_rate_sec = 60  # checking btc rate every ... seconds
     update_tx_sec = 300  # checking TX's every ... seconds.
     check_cutoff=60*60*24*30 #30 days in seconds
